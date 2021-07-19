b7548abf588a123e1a38c69b558bc2f1f4dc8c28
Michael Beamish <michael.beamish@microchip.com>
2021-07-19 10:10:38 -0700

    Added Bootstrap Vue and created basic navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navigation/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="navigation">
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="navbar-toggle-collapse">
+        <template #default="{ expanded }">
+          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+          <b-icon v-else icon="chevron-bar-down"></b-icon>
+        </template>
+      </b-navbar-toggle>
+
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item>
+            <router-link to="/">Home</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link to="/about">About</router-link>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
